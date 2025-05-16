052ca93aa533ca43cfe48a4959adb75cd5d1afd8
ImJustAPassenger <ferrauto1020@gmail.com>
2025-05-16 14:22:39 +0200

    created context for managing the cart data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
-import logoImg from '../assets/logo.jpg'
-export default function Header(){
-    return (
-        <header id="main-header">
-            <div id='title'>
-                <img src={logoImg} alt="A Restaurant" />
-                <h1>ReactFood</h1>
+import Button from "./UI/button";
+import logoImg from "../assets/logo.jpg";
+import { useContext } from "react";
+import CartContext from "../store/CartContext";
+export default function Header() {
+  const cartCtx = useContext(CartContext);
 
-            </div>
-            <nav>
-
-                <button>Cart (0)</button>
-            </nav>
-             </header>
-    )
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItem, item) => {
+    return (totalNumberOfItem + item.quantity);
+  }, 0);
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A Restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart ({totalCartItems})</Button>
+      </nav>
+    </header>
+  );
 }
