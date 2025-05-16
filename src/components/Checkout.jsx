fd54878f987daef0b7538e2b33e94b4da930a14e
ImJustAPassenger <ferrauto1020@gmail.com>
2025-05-16 17:51:09 +0200

    created hook for http request

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,22 +7,41 @@ import UserProgressContext from "../store/UserProgressContext";
 
 export default function Checkout() {
   const cartcCtx = useContext(CartContext);
-  const userProgressCtx = useContext(UserProgressContext)
+  const userProgressCtx = useContext(UserProgressContext);
   const cartTotal = cartcCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
-function handleClose(){
-    userProgressCtx.hideCheckout()
-}
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries()); //for saving all the data entered by the user
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartcCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
 
   return (
-    <Modal open={userProgressCtx.progress==='checkout'}>
-      <form >
+    <Modal open={userProgressCtx.progress === "checkout"}>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {cartTotal}</p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
@@ -30,7 +49,9 @@ function handleClose(){
           <Input label="City" type="text" id="city" />
         </div>
         <p className="modal-actions">
-          <Button type="button" textOnly  onClick={handleClose}>Close</Button>
+          <Button type="button" textOnly onClick={handleClose}>
+            Close
+          </Button>
           <Button>Submit Order</Button>
         </p>
       </form>
