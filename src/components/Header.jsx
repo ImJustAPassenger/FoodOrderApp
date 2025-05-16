db0e38dc442c0738f6964d35c2ca7b2c1ffbfe94
ImJustAPassenger <ferrauto1020@gmail.com>
2025-05-16 15:06:35 +0200

    created context for show the actual cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,20 @@ import Button from "./UI/button";
 import logoImg from "../assets/logo.jpg";
 import { useContext } from "react";
 import CartContext from "../store/CartContext";
+import UserProgressContrex from "../store/UserProgressContext"
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContrex)
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItem, item) => {
     return (totalNumberOfItem + item.quantity);
   }, 0);
+
+
+function handleShowCart(){
+    userProgressCtx.showCart()
+}
+
   return (
     <header id="main-header">
       <div id="title">
@@ -15,7 +23,7 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
